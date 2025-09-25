5cc085ae2865eb38d151e0b16603a9771f16415d
Pramod0210 <spramod320@gmail.com>
2025-09-25 21:58:11 +0530

    quota issue

--- prod_assistant/workflow/normal_generation_workflow.py
+++ prod_assistant/workflow/normal_generation_workflow.py
@@ -5,10 +5,11 @@ from langchain_core.prompts import ChatPromptTemplate
 from prompt_library.prompts import PROMPT_REGISTRY, PromptType
 from retriever.retrieval import Retriever
 from utils.model_loader import ModelLoader
-# from evaluation.ragas_eval import evaluate_context_precision, evaluate_response_relevancy
+from evaluation.ragas_eval import evaluate_context_precision, evaluate_response_relevancy
 
 retriever_obj = Retriever()
 model_loader = ModelLoader()
+
 
 def format_docs(docs) -> str:
     """Format retrieved documents into a structured text block for the prompt."""
@@ -51,6 +52,7 @@ def build_chain(query):
     )
     return chain,retrieved_contexts
 
+
 def invoke_chain(query: str, debug: bool = False):
     """Run the chain with a user query."""
     chain,retrieved_contexts = build_chain(query)
@@ -66,20 +68,51 @@ def invoke_chain(query: str, debug: bool = False):
     
     return retrieved_contexts,response
 
+
 if __name__=='__main__':
     user_query = "Can you suggest good budget iPhone under 1,00,000 INR?"
+     
+    #retriever_obj = Retriever()
+    
+    #retrieved_docs = retriever_obj.call_retriever(user_query)
+    
+    # def _format_docs(docs) -> str:
+    #     if not docs:
+    #         return "No relevant documents found."
+    #     formatted_chunks = []
+    #     for d in docs:
+    #         print(d)
+    #         meta = d.metadata or {}
+    #         formatted = (
+    #             f"Title: {meta.get('product_title', 'N/A')}\n"
+    #             f"Price: {meta.get('price', 'N/A')}\n"
+    #             f"Rating: {meta.get('rating', 'N/A')}\n"
+    #             f"Reviews:\n{d.page_content.strip()}"
+    #         )
+    #         formatted_chunks.append(formatted)
+    #     return "\n\n---\n\n".join(formatted_chunks)
+    
+    # retrieved_contexts = [_format_docs(doc) for doc in retrieved_docs]
     
     retrieved_contexts,response = invoke_chain(user_query)
     
+    #this is not an actual output this have been written to test the pipeline
+    #response="iphone 16 plus, iphone 16, iphone 15 are best phones under 1,00,000 INR."
     
-    # context_score = evaluate_context_precision(user_query,response,retrieved_contexts)
-    # relevancy_score = evaluate_response_relevancy(user_query,response,retrieved_contexts)
+    context_score = evaluate_context_precision(user_query,response,retrieved_contexts)
+    relevancy_score = evaluate_response_relevancy(user_query,response,retrieved_contexts)
     
     print("\n--- Evaluation Metrics ---")
-    print("Retrieved Contexts:")
-    for idx, context in enumerate(retrieved_contexts, 1):
-        print(f"Context {idx}:\n{context}\n")
-    print("Response from LLM:")
-    print(response)
-    # print("Context Precision Score:", context_score)
-    # print("Response Relevancy Score:", relevancy_score)
+    print("Context Precision Score:", context_score)
+    print("Response Relevancy Score:", relevancy_score)
+    
+    
+    
+# if __name__ == "__main__":
+#     try:
+#         answer = invoke_chain("can you tell me the price of the iPhone 15?")
+#         print("\n Assistant Answer:\n", answer)
+#     except Exception as e:
+#         import traceback
+#         print("Exception occurred:", str(e))
+#         traceback.print_exc()
